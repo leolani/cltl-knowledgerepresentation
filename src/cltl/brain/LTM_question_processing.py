6ed55a91eaed0abda7a93af9d01f663923abf14c
Selene Baez Santamaria <s.baezsantamaria@vu.nl>
2023-11-05 22:11:21 +0100

    Update LTM_question_processing.py
    correct grouping of answers

--- src/cltl/brain/LTM_question_processing.py
+++ src/cltl/brain/LTM_question_processing.py
@@ -12,191 +12,160 @@ def create_query(self, utterance):
     # Query subject
     if _is_empty(utterance['subject']['label']) or _is_empty(utterance['subject']['uri']):
         query = """
-        SELECT distinct ?p ?pOriginal ?s ?slabel ?o ?olabel 
-                        ?authorlabel ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue ?temporalValue
-           WHERE { 
-               BIND(<%s> AS ?p).
-               BIND(<%s> AS ?o).
+SELECT distinct ?s (group_concat(distinct ?slbl ; separator="|") as ?slabel) 
+                ?p ?pOriginal 
+                ?o (group_concat(distinct ?olbl ; separator="|") as ?olabel)
+                (group_concat(distinct ?authorlabel ; separator="|") as ?authorlabels) 
+                ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue
+WHERE { 
+   BIND(<%s> AS ?p).
+   BIND(<%s> AS ?o).
 
-               ?s ?p ?o .
-               ?pOriginal rdfs:subPropertyOf* ?p .
+    ?s ?p ?o .
+    ?pOriginal rdfs:subPropertyOf* ?p .
 
-               OPTIONAL {
-                ?s rdfs:label ?slabel .
-                ?o rdfs:label ?olabel .
-               }
+    OPTIONAL {
+        ?s rdfs:label ?slbl .
+        ?o rdfs:label ?olbl .
+    }
 
-               GRAPH ?g {
-                   ?s ?pOriginal ?o . 
-               } . 
-               
-               OPTIONAL {
-               ?g gaf:denotedBy ?m . 
-               ?m grasp:wasAttributedTo ?author . 
-               ?author rdfs:label ?authorlabel .
-               ?m grasp:hasAttribution ?att .
-               }
+    GRAPH ?g {
+        ?s ?pOriginal ?o . 
+    } . 
 
-               OPTIONAL {
-               ?att rdf:value ?certainty .
-               ?certainty rdf:type graspf:CertaintyValue .
-               ?certainty rdfs:label ?certaintyValue .
-               }
+    OPTIONAL {
+        ?g gaf:denotedBy ?m . 
+        ?m grasp:wasAttributedTo ?author . 
+        ?author rdfs:label ?authorlabel .
+        ?m grasp:hasAttribution ?att .
 
-               OPTIONAL {
-               ?att rdf:value ?polarity .
-               ?polarity rdf:type graspf:PolarityValue .
-               ?polarity rdfs:label ?polarityValue .
-               }
+        ?att rdf:value ?certainty .
+        ?certainty rdf:type graspf:CertaintyValue .
+        ?certainty rdfs:label ?certaintyValue .
 
-               OPTIONAL {
-               ?att rdf:value ?temporal .
-               ?temporal rdf:type graspf:TemporalValue .
-               ?temporal rdfs:label ?temporalValue .
-               }
+        ?att rdf:value ?polarity .
+        ?polarity rdf:type graspf:PolarityValue .
+        ?polarity rdfs:label ?polarityValue .
 
-               OPTIONAL {
-               ?att rdf:value ?emotion .
-               ?emotion rdf:type graspe:EmotionValue .
-               ?emotion rdfs:label ?emotionValue .
-               }
+        ?att rdf:value ?emotion .
+        ?emotion rdf:type graspe:EmotionValue .
+        ?emotion rdfs:label ?emotionValue .
 
-               OPTIONAL {
-               ?att rdf:value ?sentiment .
-               ?sentiment rdf:type grasps:SentimentValue .
-               ?sentiment rdfs:label ?sentimentValue .
-               }
-           }
-           """ % (utterance['predicate']['uri'],
-                  utterance['object']['uri'])
+        ?att rdf:value ?sentiment .
+        ?sentiment rdf:type grasps:SentimentValue .
+        ?sentiment rdfs:label ?sentimentValue .
+    }
+}
+GROUP BY ?s ?p ?pOriginal ?o ?author ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue
+""" % (utterance['predicate']['uri'],
+       utterance['object']['uri'])
 
     # Query complement
     elif _is_empty(utterance['object']['label']) or _is_empty(utterance['object']['uri']):
         query = """
-        SELECT distinct ?p ?pOriginal ?s ?slabel ?o ?olabel 
-                        ?authorlabel ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue ?temporalValue
-           WHERE { 
-               BIND(<%s> AS ?s).
-               BIND(<%s> AS ?p).
-            
-               ?s ?p ?o .
-               ?pOriginal rdfs:subPropertyOf* ?p .
-                
-               OPTIONAL {
-                ?s rdfs:label ?slabel .
-                ?o rdfs:label ?olabel .
-               }
+SELECT distinct ?s (group_concat(distinct ?slbl ; separator="|") as ?slabel) 
+                ?p ?pOriginal 
+                ?o (group_concat(distinct ?olbl ; separator="|") as ?olabel)
+                (group_concat(distinct ?authorlabel ; separator="|") as ?authorlabels) 
+                ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue
+WHERE { 
+   BIND(<%s> AS ?s).
+   BIND(<%s> AS ?p).      
                
-               GRAPH ?g {
-                   ?s ?pOriginal ?o . 
-               } . 
-               
-               OPTIONAL {
-               ?g gaf:denotedBy ?m . 
-               ?m grasp:wasAttributedTo ?author . 
-               ?author rdfs:label ?authorlabel .
-               ?m grasp:hasAttribution ?att .
-               }
-               
-               OPTIONAL {
-               ?att rdf:value ?certainty .
-               ?certainty rdf:type graspf:CertaintyValue .
-               ?certainty rdfs:label ?certaintyValue .
-               }
+    ?s ?p ?o .
+    ?pOriginal rdfs:subPropertyOf* ?p .
 
-               OPTIONAL {
-               ?att rdf:value ?polarity .
-               ?polarity rdf:type graspf:PolarityValue .
-               ?polarity rdfs:label ?polarityValue .
-               }
+    OPTIONAL {
+        ?s rdfs:label ?slbl .
+        ?o rdfs:label ?olbl .
+    }
 
-               OPTIONAL {
-               ?att rdf:value ?temporal .
-               ?temporal rdf:type graspf:TemporalValue .
-               ?temporal rdfs:label ?temporalValue .
-               }
+    GRAPH ?g {
+        ?s ?pOriginal ?o . 
+    } . 
 
-               OPTIONAL {
-               ?att rdf:value ?emotion .
-               ?emotion rdf:type graspe:EmotionValue .
-               ?emotion rdfs:label ?emotionValue .
-               }
+    OPTIONAL {
+        ?g gaf:denotedBy ?m . 
+        ?m grasp:wasAttributedTo ?author . 
+        ?author rdfs:label ?authorlabel .
+        ?m grasp:hasAttribution ?att .
 
-               OPTIONAL {
-               ?att rdf:value ?sentiment .
-               ?sentiment rdf:type grasps:SentimentValue .
-               ?sentiment rdfs:label ?sentimentValue .
-               }
-           }
-           """ % (utterance['subject']['uri'],
-                  utterance['predicate']['uri'])
+        ?att rdf:value ?certainty .
+        ?certainty rdf:type graspf:CertaintyValue .
+        ?certainty rdfs:label ?certaintyValue .
+
+        ?att rdf:value ?polarity .
+        ?polarity rdf:type graspf:PolarityValue .
+        ?polarity rdfs:label ?polarityValue .
+
+        ?att rdf:value ?emotion .
+        ?emotion rdf:type graspe:EmotionValue .
+        ?emotion rdfs:label ?emotionValue .
+
+        ?att rdf:value ?sentiment .
+        ?sentiment rdf:type grasps:SentimentValue .
+        ?sentiment rdfs:label ?sentimentValue .
+    }
+}
+GROUP BY ?s ?p ?pOriginal ?o ?author ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue
+""" % (utterance['subject']['uri'],
+       utterance['predicate']['uri'])
 
     # Query existence
     else:
         query = """
-        SELECT distinct ?p ?pOriginal ?s ?slabel ?o ?olabel 
-                        ?authorlabel ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue ?temporalValue
-           WHERE { 
-               BIND(<%s> AS ?s).
-               BIND(<%s> AS ?p).
-               BIND(<%s> AS ?o).
+SELECT distinct ?s (group_concat(distinct ?slbl ; separator="|") as ?slabel) 
+                ?p ?pOriginal 
+                ?o (group_concat(distinct ?olbl ; separator="|") as ?olabel)
+                (group_concat(distinct ?authorlabel ; separator="|") as ?authorlabels) 
+                ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue
+WHERE { 
+   BIND(<%s> AS ?s).
+   BIND(<%s> AS ?p).
+   BIND(<%s> AS ?o).
             
-               ?s ?p ?o .
-               ?pOriginal rdfs:subPropertyOf* ?p .
-                
-               OPTIONAL {
-                ?s rdfs:label ?slabel .
-                ?o rdfs:label ?olabel .
-               }
-               
-               GRAPH ?g {
-                   ?s ?pOriginal ?o . 
-               } . 
-               
-               OPTIONAL {
-               ?g gaf:denotedBy ?m . 
-               ?m grasp:wasAttributedTo ?author . 
-               ?author rdfs:label ?authorlabel .
-               ?m grasp:hasAttribution ?att .
-               }
-               
-               OPTIONAL {
-               ?att rdf:value ?certainty .
-               ?certainty rdf:type graspf:CertaintyValue .
-               ?certainty rdfs:label ?certaintyValue .
-               }
+   ?s ?p ?o .
+    ?pOriginal rdfs:subPropertyOf* ?p .
 
-               OPTIONAL {
-               ?att rdf:value ?polarity .
-               ?polarity rdf:type graspf:PolarityValue .
-               ?polarity rdfs:label ?polarityValue .
-               }
+    OPTIONAL {
+        ?s rdfs:label ?slbl .
+        ?o rdfs:label ?olbl .
+    }
 
-               OPTIONAL {
-               ?att rdf:value ?temporal .
-               ?temporal rdf:type graspf:TemporalValue .
-               ?temporal rdfs:label ?temporalValue .
-               }
+    GRAPH ?g {
+        ?s ?pOriginal ?o . 
+    } . 
 
-               OPTIONAL {
-               ?att rdf:value ?emotion .
-               ?emotion rdf:type graspe:EmotionValue .
-               ?emotion rdfs:label ?emotionValue .
-               }
+    OPTIONAL {
+        ?g gaf:denotedBy ?m . 
+        ?m grasp:wasAttributedTo ?author . 
+        ?author rdfs:label ?authorlabel .
+        ?m grasp:hasAttribution ?att .
 
-               OPTIONAL {
-               ?att rdf:value ?sentiment .
-               ?sentiment rdf:type grasps:SentimentValue .
-               ?sentiment rdfs:label ?sentimentValue .
-               }
-           }
-           """ % (utterance['subject']['uri'],
-                  utterance['predicate']['uri'],
-                  utterance['object']['uri'])
+        ?att rdf:value ?certainty .
+        ?certainty rdf:type graspf:CertaintyValue .
+        ?certainty rdfs:label ?certaintyValue .
+
+        ?att rdf:value ?polarity .
+        ?polarity rdf:type graspf:PolarityValue .
+        ?polarity rdfs:label ?polarityValue .
+
+        ?att rdf:value ?emotion .
+        ?emotion rdf:type graspe:EmotionValue .
+        ?emotion rdfs:label ?emotionValue .
+
+        ?att rdf:value ?sentiment .
+        ?sentiment rdf:type grasps:SentimentValue .
+        ?sentiment rdfs:label ?sentimentValue .
+    }
+}
+GROUP BY ?s ?p ?pOriginal ?o ?author ?certaintyValue ?polarityValue ?sentimentValue ?emotionValue
+""" % (utterance['subject']['uri'],
+       utterance['predicate']['uri'],
+       utterance['object']['uri'])
 
     query = self.query_prefixes + query
 
     self._log.info("Triple in question: %s", utterance['triple'])
-    # print(query)
 
     return query
